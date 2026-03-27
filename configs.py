e3fa91b67af6c4efead6f271b341b9e4dfd9dcd0
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-27 07:31:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    CHID = int(getenv("CHID", "-1000112234"))
+    API_ID = int(getenv("API_ID", "34446649"))
+    API_HASH = getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8591083411:AAH1phm791rkKiByG4kgkFB7gBhSCIjgv4Y")
+    CHID = int(getenv("CHID", "-1003515041061"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
-    LOGCHID = int(getenv("LOGCHID", "-1000112234"))
-    API = getenv("API", "abcdefu67-8dgdg")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+    LOGCHID = int(getenv("LOGCHID", "-1003515041061"))
+    API = getenv("API", "8591083411:AAH1phm791rkKiByG4kgkFB7gBhSCIjgv4Y")
 cfg = Config()
