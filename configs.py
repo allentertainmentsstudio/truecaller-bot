1f1eb4c4668741e6629114950282f9d7e4866e3f
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-27 07:34:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
     BOT_TOKEN = getenv("BOT_TOKEN", "8591083411:AAH1phm791rkKiByG4kgkFB7gBhSCIjgv4Y")
     CHID = int(getenv("CHID", "-1003515041061"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = list(map(int, getenv("SUDO", "7892805795").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
     LOGCHID = int(getenv("LOGCHID", "-1003515041061"))
     API = getenv("API", "8591083411:AAH1phm791rkKiByG4kgkFB7gBhSCIjgv4Y")
